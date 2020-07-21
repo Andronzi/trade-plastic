9f872cdcd11e7fab64482e2146b6cee95b088d3e
Andrey Radionov <47948932+Andronzi@users.noreply.github.com>
2020-07-21 13:45:32 +0500

    web site
    trade plastic web site

---
+++ trade plastic/js/index.js
@@ -0,0 +1,9 @@
+button = document.querySelector('.open');
+
+img1 = document.querySelector('.man3');
+img2 = document.querySelector('.man4');
+
+button.onclick = function() {
+    img1.style.display = 'block';
+    img2.style.display = 'block';
+}   
